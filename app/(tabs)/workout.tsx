8379e1e17a3581a801960e5e867e4f1396d6604d
ali.cebeci <ali.cebeci@stud.tu-darmstadt.de>
2025-04-11 19:57:08 +0200

    Add workout search bar for exercise and rename exercise.json

--- app/(tabs)/workout.tsx
+++ app/(tabs)/workout.tsx
@@ -1,21 +1,53 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
+import exercises from '../../assets/exercises/exercises.json'; // Corrected import path
+import { FlatList, ScrollView } from 'react-native-gesture-handler';
 
 export default function WorkoutScreen() {
+  const [text, setText] = useState('');
+  const exerciseList = exercises;
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Workout Data</Text>
-    </View>
+    <ScrollView style={{ padding: 10 }}>
+      <TextInput
+        style={styles.input}
+        placeholder="Search for exercises!"
+        onChangeText={(newText) => setText(newText)}
+        defaultValue={text}
+      />
+      <View style={styles.container}>
+        <FlatList
+          data={exerciseList.filter((exercise) =>
+            exercise.names?.en?.toLowerCase().includes(text.toLowerCase())
+          )}
+          renderItem={({ item }) => (
+            <View style={styles.itemContainer}>
+              <Text style={styles.text}>{item.names.en}</Text>
+            </View>
+          )}
+        />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#25292e',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    text: {
-      color: '#fff',
-    },
-  });
+  container: {
+    backgroundColor: '#252626',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    height: 40,
+    padding: 5,
+    borderColor: 'gray',
+    borderWidth: 1,
+    color: '#fff',
+  },
+  itemContainer: {
+    padding: 5,
+  },
+  text: {
+    color: '#fff',
+  },
+});
